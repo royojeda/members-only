f53caa2b56118ad43b9f0ae484190a35dc011ff7
Dennis Ojeda <royojeda1@gmail.com>
2022-07-05 17:08:41 +0800

    Fix Devise log-out error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "pages#home"
-  get 'pages/home'
+
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
 end
